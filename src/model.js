29f2d2bef74ee68a4f0b221faad5d6eaccce4b0a
tataqt <tata_98@ukr.net>
2020-08-21 12:09:15 +0300

    create model

---
+++ src/model.js
@@ -0,0 +1,21 @@
+export const model = [{
+        type: 'title',
+        value: 'Test Title',
+        options: {}
+    },
+    {
+        type: 'text',
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error expedita hic itaque maiores minus nulla numquam perferendis placeat sapiente!',
+        options: {}
+    },
+    {
+        type: 'textColumns',
+        value: [
+            '1 text',
+            '2 text',
+            '3 text',
+            '4 text',
+        ],
+        options: {}
+    },
+];
